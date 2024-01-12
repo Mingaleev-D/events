fe963e029b17da6a1d2eaf51bd2de2e8f8875716
admin <mingaleev.d92@mail.ru>
2024-01-12 21:48:18 +0300

    payment

--- lib/features/ui/screens/booking/booking_screen.dart
+++ lib/features/ui/screens/booking/booking_screen.dart
@@ -2,7 +2,10 @@ import 'package:auto_route/annotations.dart';
 import 'package:events/features/data/model/event.dart';
 import 'package:events/features/ui/common_widgets/positioned_button.dart';
 import 'package:events/features/ui/screens/home/widgets/event_item.dart';
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+
+import '../../../data/model/payment_mock.dart';
 
 @RoutePage()
 class BookingScreen extends StatefulWidget {
@@ -16,16 +19,19 @@ class BookingScreen extends StatefulWidget {
 
 class _BookingScreenState extends State<BookingScreen> {
   late ValueNotifier<int> _quantityNotifier;
+  late ValueNotifier<int> _paymentNotifier;
 
   @override
   void initState() {
     super.initState();
     _quantityNotifier = ValueNotifier(1);
+    _paymentNotifier = ValueNotifier(0);
   }
 
   @override
   void dispose() {
     _quantityNotifier.dispose();
+    _paymentNotifier.dispose();
     super.dispose();
   }
 
@@ -62,7 +68,27 @@ class _BookingScreenState extends State<BookingScreen> {
                         onDecrement: () => _quantityNotifier.value--,
                         onIncrement: () => _quantityNotifier.value++,
                       );
-                    })
+                    }),
+                const SizedBox(height: 48),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Payment method',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                ),
+                ...List.generate(
+                    paymentMethods.length,
+                    (index) => ValueListenableBuilder(
+                        valueListenable: _paymentNotifier,
+                        builder: (context, value, _) {
+                          return _PaymentsWidget(
+                            isSelected: index == value,
+                            text: paymentMethods[index].name,
+                            imagePath: paymentMethods[index].imagePath,
+                            onPressed: () => _paymentNotifier.value = index,
+                          );
+                        })),
               ],
             ),
             PositionedButton(
@@ -71,6 +97,53 @@ class _BookingScreenState extends State<BookingScreen> {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _PaymentsWidget extends StatelessWidget {
+  const _PaymentsWidget(
+      {super.key,
+      required this.isSelected,
+      required this.text,
+      required this.imagePath,
+      required this.onPressed});
+
+  final bool isSelected;
+  final String text;
+  final String imagePath;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: ListTile(
+        onTap: () => onPressed(),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide(
+            color: isSelected ? Colors.black : Colors.transparent,
+          ),
+        ),
+        selected: isSelected,
+        tileColor: Colors.grey.shade300,
+        selectedTileColor: Colors.grey.shade400,
+        leading: Image.asset(
+          imagePath,
+          height: 28,
+          width: 28,
+        ),
+        title: Text(
+          text,
+          style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                fontWeight: isSelected ? FontWeight.w600 : FontWeight.w400,
+              ),
+        ),
+        trailing: isSelected
+            ? const Icon(FluentIcons.checkmark_16_filled, color: Colors.black)
+            : null,
       ),
     );
   }
